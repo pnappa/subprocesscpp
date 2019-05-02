af5c9b3879be8bc47491adaf57624c4127198fd8
Patrick Nappa <patricknappa@gmail.com>
2019-05-02 13:50:01 +1000

    shitty cat simulacrum

---
+++ test_programs/cat.cpp
@@ -0,0 +1,30 @@
+/**
+ * Print out each of the files provided as arguments
+ * - is not stdin
+ * If none are provided, print stdin
+ */
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char* argv[]) {
+
+    std::vector<std::ifstream> inFiles;
+    for (int i = 1; i < argc; ++i) {
+        inFiles.push_back(std::ifstream(argv[i]));
+    }
+    // do stdin
+    if (inFiles.size() == 0) {
+        for (std::string line; std::getline(std::cin, line);) {
+            std::cout << line << std::endl;
+        }
+    } else {
+        for (std::ifstream& f : inFiles) {
+            for (std::string line; std::getline(f, line);) {
+                std::cout << line << std::endl;
+            }
+        } 
+    }
+}
+
